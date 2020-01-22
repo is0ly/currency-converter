e68c7ad8d495976af01082014b4e65393ac14720
is0ly <iljaosipov@gmail.com>
2020-01-22 21:59:16 +0300

    refactoring on promise

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 let inputRub = document.getElementById("rur"),
     inputUsd = document.getElementById("usd");
 
+// inputRub.addEventListener("input", () => {
+//     let request = new XMLHttpRequest();
+
+//     // request.open(method, url, async, login, password);
+
+//     request.open("GET", "./current.json");
+//     request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+//     request.send();
+
+//     // status
+//     // statusText
+//     //responseText / response
+//     //readyState
+//     request.addEventListener("readystatechange", function() {
+//         if (request.readyState == 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else {
+//             inputUsd.value = "something went wrong!";
+//         }
+//     });
+// });
+
 inputRub.addEventListener("input", () => {
-    let request = new XMLHttpRequest();
+    let promise = new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open("GET", "./current.json");
+        request.setRequestHeader(
+            "Content-type",
+            "application/json; charset=utf-8"
+        );
+        request.send();
 
-    // request.open(method, url, async, login, password);
-
-    request.open("GET", "./current.json");
-    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    request.send();
-
-    // status
-    // statusText
-    //responseText / response
-    //readyState
-    request.addEventListener("readystatechange", function() {
-        if (request.readyState == 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "something went wrong!";
-        }
+        request.onload = function() {
+            if (request.readyState === 4 && request.status == 200) {
+                let data = JSON.parse(request.response);
+                resolve(data);
+            } else {
+                reject();
+            }
+        };
     });
+
+    promise
+        .then(data => {
+            inputUsd.value = inputRub.value / data.usd;
+        })
+        .catch(() => {
+            inputUsd.value = "something went wrong!";
+        });
 });
